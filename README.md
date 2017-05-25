ea2d10b9f9e83d0192b7d62bfc49fc2a8d7fbf1d
lutfi <lutfi@dr.com>
2017-05-26 02:06:39 +0700

    new helper

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-Software sistem Informasi Penginapan/Villa asal2an yg dibangun oleh pemalas.
-Terbantu oleh Racode (https://belajarphp.net/racode-generator-tools-for-development-2/), termasuk di dalamnya ada Harviacode (http://harviacode.com)
+#Software sistem Informasi Penginapan/Villa asal2an yg dibangun oleh pemalas.
+Terbantu oleh Racode (https://belajarphp.net/racode-generator-tools-for-development-2/), termasuk di dalamnya ada Harviacode (http://harviacode.com).
--- application/helpers/nuris_helper.php
+++ application/helpers/nuris_helper.php
@@ -11,15 +11,3 @@ function cmb_dinamis($name,$table,$field,$pk,$selected){
     $cmb .="</select>";
     return $cmb;
 }
-
-function gen_id($kd, $table, $kolom)
-{
-  $ci =& get_instance();
-  $query = $ci->db->query("select ifnull(max(substr(".$kolom.",4)),0)+1 as max_id from ".$table."");
-  $id = $query->row_array();
-  var_dump($id);
-  $max = $id["max_id"];
-  return $kd."-".str_pad($max,4,"0",STR_PAD_LEFT);
-
-  //Penggunaan echo gen_id("kode awal", "nama tabel", "nama kolom");
-}
