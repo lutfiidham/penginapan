f70155163351dd685f7e48b0fdd3359955894574
Unknown <lutfi@dr.com>
2017-05-26 23:07:18 +0700

    Combobox dinamis

--- application/views/menu/menu_list.php
+++ application/views/menu/menu_list.php
@@ -7,19 +7,20 @@
                 <div class='box-header'>
                   <h3 class='box-title'>MENU LIST <?php echo anchor('menu/create/','Create',array('class'=>'btn btn-danger btn-sm'));?>
 		<?php echo anchor(site_url('menu/excel'), ' <i class="fa fa-file-excel-o"></i> Excel', 'class="btn btn-primary btn-sm"'); ?>
+		<?php echo anchor(site_url('menu/word'), '<i class="fa fa-file-word-o"></i> Word', 'class="btn btn-primary btn-sm"'); ?>
 		<?php echo anchor(site_url('menu/pdf'), '<i class="fa fa-file-pdf-o"></i> PDF', 'class="btn btn-primary btn-sm"'); ?></h3>
                 </div><!-- /.box-header -->
                 <div class='box-body'>
         <table class="table table-bordered table-striped" id="mytable">
             <thead>
                 <tr>
-                    <th width="80px">No</th>
-		    <th>Name</th>
+                    <th width="10px">No</th>
+		    <th>Nama Menu</th>
 		    <th>Link</th>
-		    <th>Icon</th>
-		    <th>Is Active</th>
-		    <th>Is Parent</th>
-		    <th>Action</th>
+		    <th width="30">Icon</th>
+		    <th>Aktif</th>
+		    <th>Parent</th>
+		    <th></th>
                 </tr>
             </thead>
 	    <tbody>
@@ -27,14 +28,16 @@
             $start = 0;
             foreach ($menu_data as $menu)
             {
+                $active = $menu->is_active==1?'AKTIF':'TIDAK AKTIF';
+                $parent = $menu->is_parent>1?'MAINMENU':'SUBMENU'
                 ?>
                 <tr>
 		    <td><?php echo ++$start ?></td>
 		    <td><?php echo $menu->name ?></td>
 		    <td><?php echo $menu->link ?></td>
-		    <td><?php echo $menu->icon ?></td>
-		    <td><?php echo $menu->is_active ?></td>
-		    <td><?php echo $menu->is_parent ?></td>
+		    <td><i class='<?php echo $menu->icon ?>'></i></td>
+		    <td><?php echo $active ?></td>
+		    <td><?php echo $parent ?></td>
 		    <td style="text-align:center" width="140px">
 			<?php 
 			echo anchor(site_url('menu/read/'.$menu->id),'<i class="fa fa-eye"></i>',array('title'=>'detail','class'=>'btn btn-danger btn-sm')); 
