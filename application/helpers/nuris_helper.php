a82306b19226a5ae4a55b6a7c48fe1e366f1d654
Lutfi Idham <lutfi@dr.com>
2017-05-27 16:37:16 +0700

    Perbaikan helper

--- application/helpers/nuris_helper.php
+++ application/helpers/nuris_helper.php
@@ -1,8 +1,10 @@
 <?php
 function cmb_dinamis($name,$table,$field,$pk,$selected){
     $ci = get_instance();
-    $cmb = "<select name='$name' class='form-control'>";
+    $cmb = "<select name='$name' id='$name' class='form-control'>";
     $data = $ci->db->get($table)->result();
+    $cmb .="<option value=''>-Pilih ".ucfirst($table)."-</option>";
+
     foreach ($data as $d){
         $cmb .="<option value='".$d->$pk."'";
         $cmb .= $selected==$d->$pk?" selected='selected'":'';
