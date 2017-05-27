282bced2ec1cb8c1c8bd6fce1e0799eee73fa9f2
Lutfi Idham <lutfi@dr.com>
2017-05-27 12:39:50 +0700

    parameter id generator

--- application/helpers/idham_helper.php
+++ application/helpers/idham_helper.php
@@ -1,12 +1,12 @@
 <?php
-function gen_id($kd, $table, $kolom)
+function gen_id($kd, $table, $kolom, $panjang)
 {
   $ci =& get_instance();
-  $query = $ci->db->query("select ifnull(max(substr(".$kolom.",4)),0)+1 as max_id from ".$table."");
+  $query = $ci->db->query("select ifnull(max(substr(".$kolom.",".$panjang.")),0)+1 as max_id from ".$table."");
   $id = $query->row_array();
   var_dump($id);
   $max = $id["max_id"];
-  return $kd."-".str_pad($max,4,"0",STR_PAD_LEFT);
+  return strtoupper($kd)."-".str_pad($max,4,"0",STR_PAD_LEFT);
 
-  //Penggunaan echo gen_id("kode awal", "nama tabel", "nama kolom");
+  //Penggunaan echo gen_id("kode awal", "nama tabel", "nama kolom", "panjang karakter setelah - ");
 }
